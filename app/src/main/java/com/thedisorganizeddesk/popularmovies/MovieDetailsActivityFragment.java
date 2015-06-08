72e422af89c9316a40c796201433759862b92b45
amartyakhan <amartyakhan@gmail.com>
2015-06-08 18:09:05 +0530

    Added movie poster in movie details activity fragment; Cleaned up the view for easy readability.

--- app/src/main/java/com/thedisorganizeddesk/popularmovies/MovieDetailsActivityFragment.java
+++ app/src/main/java/com/thedisorganizeddesk/popularmovies/MovieDetailsActivityFragment.java
@@ -7,7 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -43,6 +46,22 @@ public class MovieDetailsActivityFragment extends Fragment {
             rating.setText("Rating: "+movie_detail.getString("vote_average"));
             TextView releaseDate=(TextView) view.findViewById(R.id.movie_release_date);
             releaseDate.setText("Releases on: "+movie_detail.getString("release_date"));
+
+            //get the imageView for backdrop
+            ImageView backdrop= (ImageView) view.findViewById(R.id.movie_backdrop_image);
+            //setup the URL for backdrop
+            String basepath="https://image.tmdb.org/t/p/w780/";
+            String relativePath=movie_detail.getString("backdrop_path");
+            //set the image for backdrop
+            Picasso.with(getActivity()).load(basepath + relativePath).into(backdrop);
+
+            //get the imageView for backdrop
+            ImageView poster= (ImageView) view.findViewById(R.id.movie_poster);
+            //setup the URL for backdrop
+            String basepath_poster="https://image.tmdb.org/t/p/w185/";
+            String relativePath_poster=movie_detail.getString("poster_path");
+            //set the image for backdrop
+            Picasso.with(getActivity()).load(basepath_poster + relativePath_poster).into(poster);
 
         }catch (JSONException e){
             Log.e(LOG_TAG, "Error Parsing JSON: ", e);
