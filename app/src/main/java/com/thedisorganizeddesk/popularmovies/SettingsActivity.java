8aabf1e829c9ef68de6ea576624f6e9e0cc64fc5
amartyakhan <amartyakhan@gmail.com>
2015-08-15 10:10:33 +0530

    Fixing color of settings toolbar

--- app/src/main/java/com/thedisorganizeddesk/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/thedisorganizeddesk/popularmovies/SettingsActivity.java
@@ -63,18 +63,18 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
         return true;
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
-        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
-        root.addView(bar, 0); // insert at top
-        bar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
+//    @Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//
+//        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+//        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+//        root.addView(bar, 0); // insert at top
+//        bar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+//    }
 }
